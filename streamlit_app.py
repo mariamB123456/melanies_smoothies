b57fff731763e6ec229c0b41579169563ab24452
mariamB123456 <mariam.bensouda@global.nttdata.com>
2025-11-27 14:11:55 +0000

    rien

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ st.write("The name on your Smoothie will be: ", name_on_order)
 #------------------------
 cnx=st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))#show only fruit name column.
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))#show only fruit name column.
 st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
 #---------------------
