c6fd30af40b09429192678f1b96d152735d54f6e
mariamB123456 <mariam.bensouda@global.nttdata.com>
2025-11-27 14:09:18 +0000

    serach_on col aded

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,6 @@ import streamlit as st
 from snowflake.snowpark.functions import col #import a function from snowpark to show one column instead of the whole table
 import requests # this a Python package library called requests.  The requests library allows us to build and sent REST API calls
 #----------------------
-# Write directly to the app
 st.title(f" :cup_with_straw: Customize your smoothie! :cup_with_straw: ")
 st.write(  """Choose the fruits you want in your custom smoothie !  """)
 #----------------------
@@ -13,13 +12,13 @@ st.write("The name on your Smoothie will be: ", name_on_order)
 cnx=st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))#show only fruit name column.
-#st.dataframe(data=my_dataframe, use_container_width=True)
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 #---------------------
 ingredients_list = st.multiselect (
     'choose up to 5 ingredients :'
     ,my_dataframe
-    ,max_selections=5
-)
+    ,max_selections=5)
 #---------------------
 if ingredients_list: 
     ingredients_string = ''
