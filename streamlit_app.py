124a7f077a24b12259218fedda80ac1751af2540
mariamB123456 <mariam.bensouda@global.nttdata.com>
2025-11-27 13:31:31 +0000

    added the dataframe to show json data

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,4 +54,5 @@ if ingredients_list: #means if this list is not null
 #New section to import smoothie fruit information
 import requests # this a Python package library called requests.  The requests library allows us to build and sent REST API calls
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
