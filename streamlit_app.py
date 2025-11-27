b7f9977495c902ad51fff1b98b4e736913a2903e
mariamB123456 <mariam.bensouda@global.nttdata.com>
2025-11-27 13:43:06 +0000

    added fruit_chosen instead of watermellon

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list: #for every fruit in the ingredients list
         ingredients_string += fruit_chosen + ' ' #add every selection to the string
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        st.subheader(fruit_chosen+'nutrition information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)  
     
     #st.write(ingredients_list) #show finale selection into [] and with ID
