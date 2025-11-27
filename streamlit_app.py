00898b0947ef6809cecb11cfb48f0014076efbf3
mariamB123456 <mariam.bensouda@global.nttdata.com>
2025-11-27 16:03:02 +0000

    added LOC function

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,6 +30,10 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list: #for every fruit in the ingredients list
         ingredients_string += fruit_chosen + ' ' #add every selection to the string
+        
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        
         st.subheader(fruit_chosen+' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)  
